07e51772c40e1ae955b24e262649d5f8835c0a53
sainthkh <sainthkh@naver.com>
2019-01-14 18:58:49 +0900

    Set up webpack.config.js for postcss and bundling css files.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
+const webpack = require('webpack')
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const outputDir = path.join(__dirname, 'build/');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
@@ -27,7 +30,7 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          'style-loader',
+          isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
           {
             loader: 'reason-css-modules-loader',
             query: {
@@ -36,11 +39,37 @@ module.exports = {
               localIdentName: isProduction ? '[hash:base64:5]' : '[local]__[hash:base64:5]'
             }
           },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: [
+                require('postcss-import')({ addDependencyTo: webpack }),
+                require('postcss-url')(),
+                require('postcss-sass-color-functions'),
+                require('postcss-preset-env')({
+                  stage: 2,
+                }),
+                require('postcss-reporter')(),
+                require('postcss-browser-reporter')({
+                  disabled: isProduction
+                })
+              ]
+            },
+          },
         ]
       }
     ]
   },
   plugins: [
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'development', // use 'development' unless process.env.NODE_ENV is defined
+      DEBUG: false
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+      disable: !isProduction
+    }),
     new HtmlWebpackPlugin({
       template: 'src/index.html',
       inject: false
@@ -51,5 +80,11 @@ module.exports = {
     contentBase: outputDir,
     port: process.env.PORT || 3000,
     historyApiFallback: true
+  },
+  node: {
+    // workaround for webpack-dev-server issue
+    // https://github.com/webpack/webpack-dev-server/issues/60#issuecomment-103411179
+    fs: 'empty',
+    net: 'empty'
   }
 };
